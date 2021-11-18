45e226a25c33a76c3af1d88488995503704dda09
thediaf <jaharacs98@gmail.com>
2021-11-18 11:40:54 +0000

    Result

--- src/component/QuestionBox.js
+++ src/component/QuestionBox.js
@@ -1,20 +1,22 @@
 import React, {useState} from "react";
 
-const QuestionBox = ({question, options, keyt}) => {
+const QuestionBox = ({question, options, selected}) => {
     const [answer, setAnswer] = useState(options);
-    console.log(setAnswer);
+    
     let answers = [];
     for (const option of options) {
         answers.push(<button className="answerBtn">{option}</button>)
     }
     return (
-        <div className="questionBox" key= {keyt}>
+        <div className="questionBox">
             <div className="question">{question}</div>
             <div className="answers">
                 {answer.map((text, index) => (
                     <button className="answerBtn" 
                             key={index} 
-                            onClick={() => { setAnswer([text])} }
+                            onClick={() => { setAnswer([text]);
+                                    selected(text);
+                            }}
                     >
                         {text}
                     </button>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import './assets/style.css';
 import QuizService from './quizService/index';
 import QuestionBox from "./component/QuestionBox";
+import Result from "./component/Result";
 
 class QuizBee extends React.Component{
     state = {
-        questionsBank: []
+        questionsBank: [],
+        responses: 0,
+        score: 0,
     };
 
     getQuestions = () => {
@@ -21,6 +24,27 @@ class QuizBee extends React.Component{
         this.getQuestions();
     }
 
+    computeAnswer = (answer, correct) => {
+        if (answer == correct)
+        {
+            this.setState({
+                score: this.state.score + 1
+             })
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+
+        this.setState({
+            responses: 0,
+            score: 0
+        })
+    }
+
     render(){
         return (
             <div className="container">
@@ -29,9 +53,23 @@ class QuizBee extends React.Component{
                 </div>
                 <div>
                     {this.state.questionsBank.length > 0 &&
+                        this.state.responses < 5 &&
                         this.state.questionsBank.map(({question, answers, correct, questionId}) => 
-                            <h4><QuestionBox question={question} options={answers} key={questionId}/></h4>)
+                            <h4>
+                                <QuestionBox 
+                                    question={question} 
+                                    options={answers} 
+                                    key={questionId}
+                                    selected={answers => this.computeAnswer(answers, correct)}
+                                />
+                            </h4>
+                        )
                     }
+
+                    {this.state.responses === 5 ?
+                        (<Result score={this.state.score} playAgain={this.playAgain} />) : null
+                    }
+                    
                 </div>
             </div>
         );
